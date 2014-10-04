d3bc78f52ae34b6f827853d1a20746af1e797fb4
carojane <carojane@gmail.com>
2014-10-04 17:22:41 -0400

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  get 'cars/index'
-
-  get 'cars/new'
-
-  get 'manufacturers/index'
-
-  get 'manufacturers/new'
-
   resources :cars, only: [:index, :create, :new]
   resources :manufacturers, only: [:index, :create, :new]
   root 'welcome#index'
