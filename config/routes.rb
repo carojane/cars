4c4245bb58d15e4c79a945501ec6494411f3f884
carojane <carojane@gmail.com>
2014-10-04 17:02:48 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # root 'welcome#index'
+  resources :cars, only: [:index, :create, :new]
+  resources :manufacturers, only: [:index, :create, :new]
+  root 'welcome#index'
 end
