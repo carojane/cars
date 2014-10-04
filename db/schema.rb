586611d4e02f746ad73733d853e0442ef3a092cd
carojane <carojane@gmail.com>
2014-10-04 18:52:22 -0400

    uniqueness

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141004203927) do
+ActiveRecord::Schema.define(version: 20141004224627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,5 +32,7 @@ ActiveRecord::Schema.define(version: 20141004203927) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "manufacturers", ["name"], name: "index_manufacturers_on_name", unique: true, using: :btree
 
 end
